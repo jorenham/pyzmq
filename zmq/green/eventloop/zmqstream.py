1eacf36831f7abf065ba0ed56959a197ebc479c5
jorenham <jhammudoglu@gmail.com>
2026-08-27 15:00:21 +0200

    FIll in the missing type annotations in `green.zmqstream`

--- zmq/green/eventloop/zmqstream.py
+++ zmq/green/eventloop/zmqstream.py
@@ -1,9 +1,16 @@
+from __future__ import annotations
+
+from typing import TYPE_CHECKING
+
 from zmq.eventloop import zmqstream
 from zmq.green.eventloop.ioloop import IOLoop
 
+if TYPE_CHECKING:
+    from zmq.sugar.socket import Socket
+
 
 class ZMQStream(zmqstream.ZMQStream):
-    def __init__(self, socket, io_loop=None):
+    def __init__(self, socket: Socket, io_loop: IOLoop | None = None) -> None:
         io_loop = io_loop or IOLoop.instance()
         super().__init__(socket, io_loop=io_loop)
 
