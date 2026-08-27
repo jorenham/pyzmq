a74aecb66339d925b3cc1866702f4cd606ca127e
jorenham <jhammudoglu@gmail.com>
2026-08-27 14:41:48 +0200

    FIll in the missing type annotation in `eventloop.ioloop`

--- zmq/eventloop/ioloop.py
+++ zmq/eventloop/ioloop.py
@@ -29,7 +29,7 @@ from tornado.ioloop import IOLoop
 ZMQIOLoop = IOLoop
 
 
-def install():
+def install() -> None:
     """DEPRECATED
 
     pyzmq 17 no longer needs any special integration for tornado.
